666ff62cb750756543c643ec877f76570afd70a6
Luiz Paulo <lpaureliano74@gmail.com>
2019-01-23 01:24:17 -0200

    Projeto criado

---
+++ src/main/java/com/example/financeirowebapi/FinanceirowebApiApplication.java
@@ -0,0 +1,14 @@
+package com.example.financeirowebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceirowebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceirowebApiApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/example/financeirowebapi/FinanceirowebApiApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.financeirowebapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FinanceirowebApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
