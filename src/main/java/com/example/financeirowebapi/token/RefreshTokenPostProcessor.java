baa2f80ae958082f84ab7cf99d85d911d342c85f
Luiz Paulo <lpaureliano74@gmail.com>
2019-10-04 18:22:06 -0300

    Movendo refresh token para cookie

---
+++ src/main/java/com/example/financeirowebapi/token/RefreshTokenPostProcessor.java
@@ -0,0 +1,62 @@
+package com.example.financeirowebapi.token;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.http.server.ServletServerHttpResponse;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+@ControllerAdvice
+public class RefreshTokenPostProcessor implements ResponseBodyAdvice<OAuth2AccessToken>{
+
+	@Override
+	public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+		//EXECUTA beforeBodyWrite QUANDO RETORNA (TRUE) OU SEJA, QUANDO RETORNA CONFORME ABAIXO
+		return returnType.getMethod().getName().equals("postAccessToken");
+	}
+
+	@Override
+	public OAuth2AccessToken beforeBodyWrite(OAuth2AccessToken body, MethodParameter returnType,
+			MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType,
+			ServerHttpRequest request, ServerHttpResponse response) {
+		
+		//CAST PARA USAR COOKIE
+		HttpServletRequest req = ((ServletServerHttpRequest) request).getServletRequest();
+		HttpServletResponse res = ((ServletServerHttpResponse) response).getServletResponse();
+		
+		//CAST PARA USAR O MÉTODO setRefreshToken
+		DefaultOAuth2AccessToken token = (DefaultOAuth2AccessToken) body;
+		
+		String refreshToken = body.getRefreshToken().getValue();		
+		adicionarRefreshTokenNoCookie(refreshToken, req, res);
+		//MÉTODO PARA RETIRARO REFRESH TOKEN DO BODY SEGUNDO BOAS PRÁTICAS
+		removerRefreshTokenDoBody(token);
+		
+		return body;
+	}
+
+	private void removerRefreshTokenDoBody(DefaultOAuth2AccessToken token) {
+		token.setRefreshToken(null);		
+	}
+
+	private void adicionarRefreshTokenNoCookie(String refreshToken, HttpServletRequest req, HttpServletResponse res) {
+
+		Cookie refreshTokenCookie = new Cookie("refreshToken", refreshToken);		
+		refreshTokenCookie.setHttpOnly(true);
+		refreshTokenCookie.setSecure(false); // TODO: Mudar para true em producao
+		refreshTokenCookie.setPath(req.getContextPath() + "/oauth/token");
+		refreshTokenCookie.setMaxAge(2592000);
+		res.addCookie(refreshTokenCookie);
+	}
+
+}
